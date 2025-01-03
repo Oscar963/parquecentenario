9ca1d2891783c1b9af9a2b9927eb9b2ebd2dfb30
Oscar963 <oscar.apata01@gmail.com>
2025-01-03 08:28:15 -0300

    update main.yml

--- database/seeders/UserSeeders.php
+++ database/seeders/UserSeeders.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash; 
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeders extends Seeder
 {
@@ -14,35 +14,11 @@ class UserSeeders extends Seeder
             [
                 'name' => "Oscar",
                 'email' => "oscar.apata@municipalidadarica.cl",
-                'password' => Hash::make("eATmaGeneAMi"), 
+                'password' => Hash::make("eATmaGeneAMi"),
                 'id_tipo_usuarios' => 4,
                 'created_at' => now(),
                 'updated_at' => now()
-            ],
-            [
-                'name' => "Oscar Apata T",
-                'email' => "oscar.apata01@gmail.com",
-                'password' => Hash::make("oRaDENTEdgeS"), 
-                'id_tipo_usuarios' => 1,
-                'created_at' => now(),
-                'updated_at' => now()
-            ],
-            [
-                'name' => "Israel",
-                'email' => "israel@gmail.com",
-                'password' => Hash::make("colocolo123"), 
-                'id_tipo_usuarios' => 2,
-                'created_at' => now(),
-                'updated_at' => now()
-            ],
-            [
-                'name' => "Marcelo",
-                'email' => "marcelo@gmail.com",
-                'password' => Hash::make("colocolo123"), 
-                'id_tipo_usuarios' => 3,
-                'created_at' => now(),
-                'updated_at' => now()
-            ],
+            ]
         ];
 
         DB::table('users')->insert($users);
